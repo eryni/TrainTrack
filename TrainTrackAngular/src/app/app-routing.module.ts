4114d24c76db128c18071c384e223b8ba75d261b
eryni <nerine032@gmail.com>
2025-10-25 14:38:19 +0800

    Add report page layout and styling

--- TrainTrackAngular/src/app/app-routing.module.ts
+++ TrainTrackAngular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { ReportProblemComponent } from './pages/report-problem/report-problem.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'report', component: ReportProblemComponent },
+  { path: '', redirectTo: '/report', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TrainTrackAngular/src/app/app.module.ts
+++ TrainTrackAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReportProblemComponent } from './pages/report-problem/report-problem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportProblemComponent
   ],
   imports: [
     BrowserModule,
